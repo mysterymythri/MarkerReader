d5637e42940ed6a9c66ca9fbb09e63bc3dd2a843
cmorbaab <cameron.baab@menloschool.org>
2016-07-01 10:34:02 -0700

    Update CandidatePairGenerator_Levels.py

--- CandidatePairGenerator_Levels.py
+++ CandidatePairGenerator_Levels.py
@@ -1,32 +1,20 @@
 from ddlite import *
-import BiomarkerCandidateGenerator, DiseaseCandidateGenerator, pickle
+import BiomarkerCandidateGenerator, levels_extractor, pickle
 
+all_prefixes_units = [['Ym', 'yottameter'], ['Zm', 'zettameter'], ['Em', 'exameter'], ['Pm', 'petameter'], ['Tm', 'terameter'], ['Gm', 'gigameter'], ['Mm', 'megameter'], ['km', 'kilometer'], ['hm', 'hectometer'], ['dam', 'dekameter'], ['dm', 'decimeter'], ['cm', 'centimeter'], ['\\u03bcm', 'micrometer'], ['nm', 'nanometer'], ['pm', 'picometer'], ['fm', 'femtometer'], ['am', 'attometer'], ['zm', 'zeptometer'], ['ym', 'yoctometer'], ['Ym2', 'square yottameter'], ['Zm2', 'square zettameter'], ['Em2', 'square exameter'], ['Pm2', 'square petameter'], ['Tm2', 'square terameter'], ['Gm2', 'square gigameter'], ['Mm2', 'square megameter'], ['km2', 'square kilometer'], ['hm2', 'square hectometer'], ['dam2', 'square dekameter'], ['dm2', 'square decimeter'], ['cm2', 'square centimeter'], ['\\u03bcm2', 'square micrometer'], ['nm2', 'square nanometer'], ['pm2', 'square picometer'], ['fm2', 'square femtometer'], ['am2', 'square attometer'], ['zm2', 'square zeptometer'], ['ym2', 'square yoctometer'], ['Ym2', 'cubic yottameter'], ['Zm2', 'cubic zettameter'], ['Em2', 'cubic exameter'], ['Pm2', 'cubic petameter'], ['Tm2', 'cubic terameter'], ['Gm2', 'cubic gigameter'], ['Mm2', 'cubic megameter'], ['km2', 'cubic kilometer'], ['hm2', 'cubic hectometer'], ['dam2', 'cubic dekameter'], ['dm2', 'cubic decimeter'], ['cm2', 'cubic centimeter'], ['\\u03bcm2', 'cubic micrometer'], ['nm2', 'cubic nanometer'], ['pm2', 'cubic picometer'], ['fm2', 'cubic femtometer'], ['am2', 'cubic attometer'], ['zm2', 'cubic zeptometer'], ['ym2', 'cubic yoctometer'], ['YL', 'yottaliter'], ['ZL', 'zettaliter'], ['EL', 'exaliter'], ['PL', 'petaliter'], ['TL', 'teraliter'], ['GL', 'gigaliter'], ['ML', 'megaliter'], ['kL', 'kiloliter'], ['hL', 'hectoliter'], ['daL', 'dekaliter'], ['dL', 'deciliter'], ['cL', 'centiliter'], ['\\u03bcL', 'microliter'], ['nL', 'nanoliter'], ['pL', 'picoliter'], ['fL', 'femtoliter'], ['aL', 'attoliter'], ['zL', 'zeptoliter'], ['yL', 'yoctoliter'], ['Yg', 'yottagram'], ['Zg', 'zettagram'], ['Eg', 'exagram'], ['Pg', 'petagram'], ['Tg', 'teragram'], ['Gg', 'gigagram'], ['Mg', 'megagram'], ['kg', 'kilogram'], ['hg', 'hectogram'], ['dag', 'dekagram'], ['dg', 'decigram'], ['cg', 'centigram'], ['\\u03bcg', 'microgram'], ['ng', 'nanogram'], ['pg', 'picogram'], ['fg', 'femtogram'], ['ag', 'attogram'], ['zg', 'zeptogram'], ['yg', 'yoctogram']]
 
 def doEverything():
     parser = DocParser('AGR2_blood_biomarker.txt', ftreader=TextReader())
     sentences = parser.parseDocSentences()
 
     BM = BiomarkerCandidateGenerator.generateBiomarkerCandidates()
-    DM = DiseaseCandidateGenerator.generateDiseaseCandidates()
+    LM = levels_extractor.levels_extractor()
 
-    possiblePairs = Relations(sentences, BM, DM)
+    possiblePairs = Relations(sentences, BM, LM)
     feats = possiblePairs.extract_features()
     otherModel = DDLiteModel(possiblePairs, feats)
-    keyWords = ["associate", "express", "marker", "biomarker", "elevated", "decreased",
-                "correlation", "correlates", "found", "diagnose", "variant", "appear",
-                "connect", "relate", "exhibit", "indicate", "signify", "show", "demonstrate",
-                "reveal", "suggest", "evidence", "elevation", "indication", "diagnosis",
-                "variation", "modification", "suggestion", "link", "derivation", "denote",
-                "denotation", "demonstration", "magnification", "depression", "boost", "level",
-                "advance", "augmentation", "lessening", "enhancement", "expression", "buildup",
-                "diminishing", "diminishment", "reduction", "drop", "dwindling", "lowering"]
     negationWords = ["not", "nor", "neither"]
 
-    def presenceOfNot(m):
-        for word in negationWords:
-            if (word in m.post_window1('lemmas', 20)) and (word in m.pre_window2('lemmas', 20)):
-                return True
-        return False
 
     # 1
     def LF_distance(m):
@@ -34,7 +22,6 @@ def doEverything():
         # print m.dep_labels
         distance = abs(m.e2_idxs[0] - m.e1_idxs[0])
         if distance < 8:
-            # print "RETURNING ONE"
             return 0
         else:
             return -1
