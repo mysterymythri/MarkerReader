45cb8d9bafd41c0f0d8bc83f865a91074e0efb32
cmorbaab <cameron.baab@menloschool.org>
2016-07-01 09:43:04 -0700

    Update levels_extractor.py

--- levels_extractor.py
+++ levels_extractor.py
@@ -1,22 +1,16 @@
 from ddlite import *
 import re
 
-mass_units = ['kg','g','ng']
-volume_units = ['L','dL','cL','mL','nL']
+prefixes = [['Y','yotta'],['Z','zetta'],['E','exa'],['P','peta'],['T','tera'],['G','giga'],['M','mega'],['k','kilo'],['h','hecto'],
+            ['da','deka'],['d','deci'],['c','centi'],['\u03bc','micro'],['n','nano'],['p','pico'],['f','femto'],['a','atto'],
+            ['z','zepto'],['y','yocto']]
+length_unit = ['m','meter']
+area_unit = ['m2','square ','meter']
+volume_unit = ['m3','cubic ','meter']
+liquid_volume_unit = ['L','liter']
+mass_unit = ['g','gram']
 
-from itertools import izip_longest
-def grouper(n, iterable, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx
-    args = [iter(iterable)] * n
-    return izip_longest(fillvalue=fillvalue, *args)
-
-n = 300
-
-with open('AGR2_blood_biomarker.txt') as f:
-    for i, g in enumerate(grouper(n, f, fillvalue=''), 1):
-        with open('small_file_{0}'.format(i * n), 'w') as fout:
-            fout.writelines(g)
+all_prefixes_units = [['Ym', 'yottameter'], ['Zm', 'zettameter'], ['Em', 'exameter'], ['Pm', 'petameter'], ['Tm', 'terameter'], ['Gm', 'gigameter'], ['Mm', 'megameter'], ['km', 'kilometer'], ['hm', 'hectometer'], ['dam', 'dekameter'], ['dm', 'decimeter'], ['cm', 'centimeter'], ['\\u03bcm', 'micrometer'], ['nm', 'nanometer'], ['pm', 'picometer'], ['fm', 'femtometer'], ['am', 'attometer'], ['zm', 'zeptometer'], ['ym', 'yoctometer'], ['Ym2', 'square yottameter'], ['Zm2', 'square zettameter'], ['Em2', 'square exameter'], ['Pm2', 'square petameter'], ['Tm2', 'square terameter'], ['Gm2', 'square gigameter'], ['Mm2', 'square megameter'], ['km2', 'square kilometer'], ['hm2', 'square hectometer'], ['dam2', 'square dekameter'], ['dm2', 'square decimeter'], ['cm2', 'square centimeter'], ['\\u03bcm2', 'square micrometer'], ['nm2', 'square nanometer'], ['pm2', 'square picometer'], ['fm2', 'square femtometer'], ['am2', 'square attometer'], ['zm2', 'square zeptometer'], ['ym2', 'square yoctometer'], ['Ym2', 'cubic yottameter'], ['Zm2', 'cubic zettameter'], ['Em2', 'cubic exameter'], ['Pm2', 'cubic petameter'], ['Tm2', 'cubic terameter'], ['Gm2', 'cubic gigameter'], ['Mm2', 'cubic megameter'], ['km2', 'cubic kilometer'], ['hm2', 'cubic hectometer'], ['dam2', 'cubic dekameter'], ['dm2', 'cubic decimeter'], ['cm2', 'cubic centimeter'], ['\\u03bcm2', 'cubic micrometer'], ['nm2', 'cubic nanometer'], ['pm2', 'cubic picometer'], ['fm2', 'cubic femtometer'], ['am2', 'cubic attometer'], ['zm2', 'cubic zeptometer'], ['ym2', 'cubic yoctometer'], ['YL', 'yottaliter'], ['ZL', 'zettaliter'], ['EL', 'exaliter'], ['PL', 'petaliter'], ['TL', 'teraliter'], ['GL', 'gigaliter'], ['ML', 'megaliter'], ['kL', 'kiloliter'], ['hL', 'hectoliter'], ['daL', 'dekaliter'], ['dL', 'deciliter'], ['cL', 'centiliter'], ['\\u03bcL', 'microliter'], ['nL', 'nanoliter'], ['pL', 'picoliter'], ['fL', 'femtoliter'], ['aL', 'attoliter'], ['zL', 'zeptoliter'], ['yL', 'yoctoliter'], ['Yg', 'yottagram'], ['Zg', 'zettagram'], ['Eg', 'exagram'], ['Pg', 'petagram'], ['Tg', 'teragram'], ['Gg', 'gigagram'], ['Mg', 'megagram'], ['kg', 'kilogram'], ['hg', 'hectogram'], ['dag', 'dekagram'], ['dg', 'decigram'], ['cg', 'centigram'], ['\\u03bcg', 'microgram'], ['ng', 'nanogram'], ['pg', 'picogram'], ['fg', 'femtogram'], ['ag', 'attogram'], ['zg', 'zeptogram'], ['yg', 'yoctogram']]
 
 
 
@@ -28,29 +22,25 @@ def parseDocIntoWords():
     words = []
     for sentence in list:
         words.append(sentence)
+        print sentence
 
     return words
+#(?<=[\s\W])[0-9]+[-,\.]?[0-9]+\s(?=[a-zA-Z,\.])'
 
 def levelsGenerator():
     words = parseDocIntoWords()
-    print words
-    normal_syntax_regex = RegexNgramMatch(label='syntax', regex_pattern=r'(?<=[\s\W])[0-9]+[-,\.]?[0-9]+\s(?=[a-zA-Z,\.])', ignore_case=False, match_attrib='words')
-    range_syntax_regex = RegexNgramMatch(label='syntax', regex_pattern=r'[0-9]+\s?[\W^;]?[0-9]+\s[()]{1}[\W0-9^;]+[()]{1}', ignore_case=False, match_attrib='text')
+
+    normal_syntax_regex = RegexNgramMatch(label='normal', regex_pattern=r'(?<=[^a-zA-Z:;])[0-9]+[-,\.]?[0-9]+', ignore_case=False, match_attrib='lemmas')
+    range_syntax_regex = RegexNgramMatch(label='range', regex_pattern=r'[0-9]+[\W^;]?[0-9]+\s[()][^\sa-zA-Z()]+[()]', ignore_case=False, match_attrib='text')
     CE = Union(normal_syntax_regex,range_syntax_regex)
-    E = Entities(words, range_syntax_regex)
-    for e in E:
-        print e.mention(attribute='words')
+    for sentence in words:
+        obj = [sentence]
+        E = Entities(obj, CE)
+        for e in E:
+            print e.mention(attribute='words')
 
 
 levelsGenerator()
 
-# Normal 10 mg/l: (?<=\s)[0-9]+\.?[0-9]+\s[a-zA-Z]{1,2}\/?[a-zA-Z]{0,2}
 
 
-#[0-9]+ \W[LRB]{3,}\W[0-9-]+\W[RRB]{3,}\W [a-zA-Z]+\/?[a-zA-Z]+
-
-#(?<=\s)[0-9]+[.]?[0-9]+\s[a-zA-Z]{1,2}\/?[a-zA-Z]{0,2}|(?<=\s)[0-9]+[.]?[0-9]\s(.+)\s[a-zA-Z]{1,2}\/?[a-zA-Z]{0,2}
-
-#[0-9]+\s?[\W^;]?[0-9]+\s[()]{1}[\W0-9^;]+[()]{1}
-
-#current
