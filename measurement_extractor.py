3dfbaebdd89d3a9e6fbeb34221eb749537fcc1f4
cmorbaab <cameron.baab@menloschool.org>
2016-07-06 14:54:29 -0700

    Update measurement_extractor.py

--- measurement_extractor.py
+++ measurement_extractor.py
@@ -1,6 +1,18 @@
 from ddlite import *
 
+from itertools import izip_longest
+def grouper(n, iterable, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx
+    args = [iter(iterable)] * n
+    return izip_longest(fillvalue=fillvalue, *args)
 
+n = 300
+
+with open('AGR2_blood_biomarker.txt') as f:
+    for i, g in enumerate(grouper(n, f, fillvalue=''), 1):
+        with open('small_file_{0}'.format(i * n), 'w') as fout:
+            fout.writelines(g)
 
 def parseDocIntoWords():
     filename = "AGR2_blood_biomarker.txt"
@@ -10,23 +22,20 @@ def parseDocIntoWords():
     words = []
     for sentence in list:
         words.append(sentence)
-        print sentence
-
+    print words
     return words
 
+
 def measurementGenerator(levels_entities):
-  
-  words = parseDocIntoWords()
+    words = parseDocIntoWords()
+    sentence_candidates = []
     all_levels_idxs = []
     for level in levels_entities:
-      level_idxs = [level.sent_id, level.idxs[0]]
-      all_levels_idxs.append(level_idx)
-      
-    
-    
-    normal_syntax_regex = RegexNgramMatch(label='normal', regex_pattern=r'(?<=[^a-zA-Z:;])[0-9]+[-,\.]?[0-9]+', ignore_case=False, match_attrib='lemmas')
-    E = Entities(obj, CE)
+        level_idxs = [level.sent_id, level.idxs[0]]
+        all_levels_idxs.append(level_idxs)
+        sentence_candidates.append(words[level.sent_id])
 
+    noun_regex = RegexNgramMatch(label='Nouns', regex_pattern=r'[A-Z]?NN[A-Z]?', ignore_case=True, match_attrib='poses')
 
-levelsGenerator()
-
+    E = Entities(sentence_candidates, noun_regex)
+    return E
